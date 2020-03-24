9bd148d23561c89464ba244676ebf07a0239e913
designMoreWeb <divystolia@gmail.com>
2020-03-23 22:59:40 -0400

    updated files

--- cypress/integration/examples/form.spec.js
+++ cypress/integration/examples/form.spec.js
@@ -2,5 +2,17 @@ describe("Form test", () => {
     it("Can fill the form", () => {
         cy.visit("/");
         cy.get("form");
+
+        cy.get('input[name="name"]')
+            .type("Dan")
+            .should("have.value", "Dan");
+
+        cy.get('input[name="email"]')
+            .type("dan@smith.ninja")
+            .should("have.value", "dan@smith.ninja");
+
+        cy.get("textarea")
+            .type("Hello World")
+            .should("have.value", "Hello World");
     });
 });//end test
